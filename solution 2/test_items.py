34617fd24c3f68668082a5ab6d7a70d2f3018056
IamGOOD <rukshenas1990@gmail.com>
2023-08-09 23:59:26 -0400

    assert bug fixed

--- solution 2/test_items.py
+++ solution 2/test_items.py
@@ -4,4 +4,4 @@ from selenium.webdriver.common.by import By
 class TestPageElements():
     def test_basket_bottom_solution2(self, browser):
         browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-        assert len(browser.find_elements(By.CLASS_NAME, 'add-to-basket')), '"add to cart" button was not found'
+        assert len(browser.find_elements(By.CLASS_NAME, 'add-to-basket')) == 1, '"add to cart" button was not found'
